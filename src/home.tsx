fb65fd4011d6cfe702c14cfa999a3dbd3d75623b
Gina Zhou <ginaz@scribd.com>
2021-04-09 14:58:41 -0700

    feat: add instruction screenshots and update some styling

--- src/home.tsx
+++ src/home.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import figmaExportImage from '../assets/images/figmaExport.png';
+import figmaFrameImage from '../assets/images/figmaFrame.png';
+
 const Home = () => {
   const [images, setImages] = useState([]);
 
@@ -11,38 +14,50 @@ const Home = () => {
 
   return (
     <div className="homeWrapper">
-      <div>
-        <h2 className="homeTitle">eye spy</h2>
-        <div className="uploadInstructions">
-          <div>how to use the tool:</div>
-          <ol>
-            <li>
-              export the selected Figma artboard in jpg or png format (upper
-              right in Figma)
-            </li>
-            <li>
-              upload the exported file below (note: multiple images will open
-              multiple windows)
-            </li>
-            <li>drag and position the window over your current work</li>
-            <li>compare work with specs</li>
-            <li>fix code as needed</li>
-          </ol>
-        </div>
-        <h3 className="uploadText">upload image(s):</h3>
-        <input
-          accept="image/png, image/jpeg, image/jpg"
-          id="file-selector"
-          multiple
-          onChange={onChange}
-          type="file"
-        />
+      <h2 className="homeTitle">eye spy</h2>
+      <div className="uploadInstructions">
+        <div>how to use the tool:</div>
+        <ol>
+          <li>
+            <div>select the desired Figma frame you want to use</div>
+            <img
+              className="figmaImage"
+              src={figmaFrameImage}
+              alt="figma_frame_example"
+            />
+          </li>
+          <li>
+            <div>
+              export the selected Figma frame in jpg or png format (upper-right
+              hand corner in Figma)
+            </div>
+            <img
+              className="figmaImage"
+              src={figmaExportImage}
+              alt="figma_export_example"
+            />
+          </li>
+          <li>
+            upload the exported file below (note: multiple images will open
+            multiple windows)
+          </li>
+          <li>drag and position the window over your current work</li>
+          <li>compare work with specs</li>
+          <li>fix code as needed</li>
+        </ol>
       </div>
-      <div>
-        {images.map((image: any) => (
-          <img key={image.name} src={image.path} alt={image.name} />
-        ))}
-      </div>
+      <h3>upload image(s):</h3>
+      <input
+        accept="image/png, image/jpeg, image/jpg"
+        id="file-selector"
+        multiple
+        onChange={onChange}
+        type="file"
+      />
+      {images.map((image: any) => (
+        <img key={image.name} src={image.path} alt={image.name} />
+      ))}
+
       <Link to="/settings">Go to overlay settings</Link>
     </div>
   );
